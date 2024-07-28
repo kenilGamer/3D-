c43b2c50097be8c6cc0522d0215062bb8f993e46
kenil Gamer <kenilk677@gmail.com>
2024-07-28 13:07:04 +0530

    ContactShadows />

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,43 @@
-import { OrbitControls } from "@react-three/drei";
+import { ContactShadows, Environment, OrbitControls, Sky } from "@react-three/drei";
+import { Avatar } from "./avatar";
+import { Avatar2 } from "./Kenil";
+import { useControls } from "leva";
 
 export const Experience = () => {
+  const {animation} = useControls({
+    animation:{
+      value: "Typing",
+      options: ["Typing","Falling","Standing"]
+    }
+  });
   return (
     <>
       <OrbitControls />
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
+      <Sky/>
+      <Environment preset="sunset"/>
+      <group position-y={-1}>
+      <ContactShadows
+          opacity={0.62}
+          scale={10}
+          blur={1}
+          far={10}
+          resolution={256}
+          color="#000000"
+        />
+      <Avatar2 animation={animation} />
+      {
+        animation === "Typing" &&  <mesh scale={[0.8,0.5,0.8]} position-y={0.20}>
+        <boxGeometry  />
+        <meshStandardMaterial color="white"/>
       </mesh>
+      }
+   
+      {/* <ambientLight intensity={1}  /> */}
+      <mesh receiveShadow scale={5} rotation-x={-Math.PI * 0.5} position-y={-0.001}>
+        <planeGeometry/>
+        <meshStandardMaterial color="white"/>
+      </mesh>
+      </group>
     </>
   );
 };
